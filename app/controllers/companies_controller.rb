6d6f4ca28206d02c79a00e9df02c8d594ce24304
sahej <ssaluja@ncsu.edu>
2018-10-10 23:49:20 -0400

    foo345

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -28,7 +28,9 @@ class CompaniesController < ApplicationController
 
     respond_to do |format|
       if @company.save
-        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.html { if session[:role] == 'Realtor'
+                        redirect_to "/realtors/update_profile/#{@company.id}"
+                      else redirect_to @company, notice: 'Company was successfully created.' end}
         format.json { render :show, status: :created, location: @company }
           else
         format.html { render :new }
