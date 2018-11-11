2976fa51748eb353de03a234a60ff88a7e8004dd
Raveena <rmdcosta@ncsu.edu>
2018-11-11 00:19:41 -0500

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,7 @@ company.size = 2000
 company.year = 1990
 company.revenue = 200000
 company.synopsis = 'Test'
-
-house_hunter = HouseHunter.new
-house_hunter.email = 'admin@gmail.com'
-house_hunter.name = 'Admin'
-house_hunter.password = 'abc123'
-house_hunter.password_confirmation = 'abc123'
-house_hunter.phone = 9845785125
-house_hunter.preferred = 'email'
-house_hunter.save!
+company.save!
 
 realtor = Realtor.new
 realtor.email = 'admin@gmail.com'
@@ -31,3 +23,4 @@ realtor.password = 'abc123'
 realtor.password_confirmation = 'abc123'
 realtor.phone = 9845782563
 realtor.company_id = 1
+realtor.save!
